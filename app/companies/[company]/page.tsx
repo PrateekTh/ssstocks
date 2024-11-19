99cba4b4bc6fab7d1bf1af2c129aaf6315ca43f4
PrateekTh <prateek33thakur@gmail.com>
2024-11-20 01:09:32 +0530

    Added Dynamic Pages for Companies

--- app/companies/[company]/page.tsx
+++ app/companies/[company]/page.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-function CompanyList() {
+function CompanyPage({params}: {params: {company:string}}) {
+
+    
     return (
         <div className='flex p-5 md:p-10 mx-auto w-screen'>
             <div className='text-3xl'>
-                List of Companies page
+                Individual Company Details Page for {params.company}
             </div>
         </div>
-  )
+    )
 }
 
-export default CompanyList
+export default CompanyPage
