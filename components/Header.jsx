12a92a54a6e8a393871a4d6c03470b3efb4c2174
PrateekTh <prateek33thakur@gmail.com>
2024-11-20 01:16:22 +0530

    Added Header Routes

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Header() {
   return (
     <header className='p-4 mx-2 border-b border-gray-500 flex gap-9 items-center'>
-        <div className='text-xl font-bold italic'>ssstocks</div>
+        <div className='text-xl font-bold italic'><Link href='/'>ssstocks</Link></div>
         <div className='flex-1'> </div>
-        <div>companies</div>
-        <div>github</div>
+        <div><Link href='/companies'>companies</Link></div>
+        <div><Link href='https://github.com/PrateekTh/ssstocks' rel="noopener noreferrer" target="_blank">github</Link></div>
     </header>
   )
 }
